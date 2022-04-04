d33ac7cc304fff224a8129346ae3125b4454181c
Baraka-Mugisha <mugishaje@gmail.com>
2022-04-04 15:19:48 +0200

    auth features added java sevlet for login added java servlet for signup feature added profile servlet

---
+++ src/main/java/com/jeanpierre/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.jeanpierre;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println(123);
+
+
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Gson gson = new Gson();
+		
+		try {
+			
+			String username = req.getParameter("username");
+			String password = req.getParameter("password");
+			
+			User user = VolatileDB.fetch(username);
+			
+			boolean isLogin = user.Login(username, password);
+			
+			
+			if(!isLogin) {
+				resp.setContentType("application/json");
+				resp.setCharacterEncoding("UTF-8");
+				resp.getWriter().println(gson.toJson(new ApiResponse<Object>(404, "user not found", null)));
+			}
+			else {
+				resp.setContentType("application/json");
+				resp.setCharacterEncoding("UTF-8");
+				resp.getWriter().println(gson.toJson(new ApiResponse<Object>(200, "Successfull login", user)));
+			}
+			
+		} catch (Exception e) {
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			resp.getWriter().println(gson.toJson(new ApiResponse<Object>(404, "user not found", null)));
+		}
+		
+	}
+
+}
